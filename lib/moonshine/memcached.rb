9582f532a7df21eacc222f701c3324653a8b4968
Rails Machine <ops@railsmachine.com>
2010-06-01 13:42:21 -0700

    Fix a really annoying issue that prevented deploys from succeeding if the user wanted to use the memcached gem instead of memcache-client.

--- lib/moonshine/memcached.rb
+++ lib/moonshine/memcached.rb
@@ -3,31 +3,33 @@ module Moonshine
 
     # Define options for this plugin via the <tt>configure</tt> method
     # in your application manifest:
-    #
+    #    
     #   configure(:memcached => {:foo => true})
-    #
+    #    
     # Then include the plugin and call the recipe(s) you need:
-    #
-    #  plugin :memcached
-    #  recipe :memcached
-    def memcached(options = {})
+    #    
+    #  recipe :memcached    def memcached(options = {})
       options = { :enable_on_boot => true }.merge(options)
 
-      package 'memcached', :ensure => :installed
-      service 'memcached', :ensure => :running, :enable => options[:enable_on_boot], :require => package('memcached')
+      exec 'apt-get install -q -y --force-yes memcached',
+        :before => service('memcached'),
+        :alias => 'memcached package',
+        :unless => 'dpkg -l | grep memcached'
+
+      service 'memcached', :ensure => :running, :enable => options[:enable_on_boot], :require => exec('memcached package')
     
       file '/etc/memcached.conf', 
         :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'memcached.conf.erb'), binding),
         :mode => '644',
-        :require => package('memcached'),
+        :require => exec('memcached package'),
         :notify => service('memcached')
     
       # install client gem if specified. otherwise, use version bundled with rails.
       if options[:client]
-        gem 'memcache-client', :require => package('memcached'), :version => options[:client]
-      end
+        gem 'memcache-client', :require => exec('memcached package'), :version => options[:client]
+      end  
     
-    end
+    end  
   
-  end
+  end  
 end
