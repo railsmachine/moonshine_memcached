22823def84b88760eef20025e8541309fb26841a
Will Farrington <will@railsmachine.com>
2010-06-01 16:50:55 -0400

    Fix previous commit. GitHub web interface is not nice for editing.

--- lib/moonshine/memcached.rb
+++ lib/moonshine/memcached.rb
@@ -1,14 +1,15 @@
 module Moonshine
   module Memcached
-
     # Define options for this plugin via the <tt>configure</tt> method
     # in your application manifest:
-    #    
+    #
     #   configure(:memcached => {:foo => true})
-    #    
+    #
     # Then include the plugin and call the recipe(s) you need:
-    #    
-    #  recipe :memcached    def memcached(options = {})
+    #
+    #  recipe :memcached
+
+    def memcached(options = {})
       options = { :enable_on_boot => true }.merge(options)
 
       exec 'apt-get install -q -y --force-yes memcached',
@@ -17,19 +18,17 @@ module Moonshine
         :unless => 'dpkg -l | grep memcached'
 
       service 'memcached', :ensure => :running, :enable => options[:enable_on_boot], :require => exec('memcached package')
-    
-      file '/etc/memcached.conf', 
+
+      file '/etc/memcached.conf',
         :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', 'memcached.conf.erb'), binding),
         :mode => '644',
         :require => exec('memcached package'),
         :notify => service('memcached')
-    
+
       # install client gem if specified. otherwise, use version bundled with rails.
       if options[:client]
         gem 'memcache-client', :require => exec('memcached package'), :version => options[:client]
-      end  
-    
-    end  
-  
-  end  
+      end
+    end
+  end
 end
